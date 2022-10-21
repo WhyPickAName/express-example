26ac474124babe054f9f372f015039289126edc7
james.scales <james.scales@airsweb.com>
2022-10-21 11:54:10 +0100

    add port listener

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
